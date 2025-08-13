af39a32b5704a893c4c91985fad517a7e0c03ba4
suyal07 <suyal.parth@gmail.com>
2025-08-13 15:53:33 +0530

    Add DDL scripts and enterprise data assets (DDL: location/org_hierarchy/source_workers/target_workers; Data: EmployeeInfo.xlsx, location/org_hierarchy CSVs)

---
+++ ddl scripts/location.sql
@@ -0,0 +1,7 @@
+CREATE TABLE source.location (
+    [Id] varchar(255) PRIMARY KEY, 
+    [City] Varchar(255),
+    [Country] VARCHAR(255),
+    [LocationType] VARCHAR(255),
+    [Address] VARCHAR(255)
+    );
---
+++ ddl scripts/org_hierarchy.sql
@@ -0,0 +1,11 @@
+CREATE TABLE source.org_hierarchy (
+    [CostCenterID] INT PRIMARY KEY,
+    [CostCenterName] VARCHAR(255),
+    [DivisionID] VARCHAR(255),
+    [DivisionName] VARCHAR(255),
+    [DirectorateID] VARCHAR(255),
+    [DirectorateName] VARCHAR(255),
+    [BusinessAreaID] VARCHAR(255),
+    [BusinessGroupID] VARCHAR(255),
+    [BusinessGroupName] VARCHAR(255)
+);
